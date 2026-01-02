ab948d164361a8a76dedd004b93fa54659ec7971
David Reay <david@awsbot.com>
2026-01-02 09:25:15 +0000

    included missing import

--- Gnubg/Unity/Editor/GnubgInstaller.cs
+++ Gnubg/Unity/Editor/GnubgInstaller.cs
@@ -5,6 +5,7 @@ using UnityEditor;
 using UnityEngine;
 using UnityEngine.Networking;
 using System.Diagnostics;
+using System.Runtime.InteropServices;
 using Debug = UnityEngine.Debug;
 
 [InitializeOnLoad]
